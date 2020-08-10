aeeb3930275fa5b1d21955eb23c3289a2346d4fa
karan <kmessilj@gmail.com>
2020-08-11 00:36:13 +0530

    Deepspeech added.

--- speech_text.py
+++ speech_text.py
@@ -23,9 +23,9 @@ def write_to_file(text):
     except FileExistsError:
         pass
     os.chdir(text_folder)
-    name = file_name[1] + '.txt'
+    name = file_name[0] + '.txt'
     file_text = open(name, 'a+')
-    file_text.write(text+'\n')
+    file_text.write(text + '\n')
     file_text.close()
     os.chdir('..')
     print(f'{name} file generated with text...')
@@ -48,7 +48,7 @@ def long_files_transcript(song):
     for i, chunk in enumerate(song[::chunk_size]):
         file_name_chunk = "obama-%s.wav" % i
         with open(file_name_chunk, "wb") as f:
-            single_chunk = AudioSegment.silent(10)+chunk+AudioSegment.silent(10)
+            single_chunk = AudioSegment.silent(10) + chunk + AudioSegment.silent(10)
             single_chunk.export(f, bitrate='192k', format="wav")
         with sr.AudioFile(file_name_chunk) as source:
             audio = r.record(source)
@@ -64,18 +64,18 @@ if __name__ == '__main__':
     extension = ('wav', 'mp3')
 
     # file 1
-    # song_file = audio_folder + '/' + file_name[0] + '.' + extension[0]
-    # sample_audio = sr.AudioFile(song_file)
-    # with sample_audio as source:
-    #     audio = r.record(source)
-    #     extract_text(audio)
+    song_file = audio_folder + '/' + file_name[0] + '.' + extension[0]
+    sample_audio = sr.AudioFile(song_file)
+    with sample_audio as source:
+        audio = r.record(source)
+        extract_text(audio)
 
     # file 2
-    song_file = audio_folder + '/' + file_name[1] + '.' + extension[1]
-    try:
-        song_file_wav = AudioSegment.from_mp3(song_file)
-        song_file_wav.export(audio_folder + '/obama.wav', bitrate='192k', format="wav")
-        song = AudioSegment.from_wav(audio_folder + '/obama.wav')
-        long_files_transcript(song)
-    except FileNotFoundError as e:
-        print(f'Custom ==> {e}')
+    # song_file = audio_folder + '/' + file_name[1] + '.' + extension[1]
+    # try:
+    #     song_file_wav = AudioSegment.from_mp3(song_file)
+    #     song_file_wav.export(audio_folder + '/obama.wav', bitrate='192k', format="wav")
+    #     song = AudioSegment.from_wav(audio_folder + '/obama.wav')
+    #     long_files_transcript(song)
+    # except FileNotFoundError as e:
+    #     print(f'Custom ==> {e}')
